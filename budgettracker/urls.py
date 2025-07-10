4ecc7a5a7275f253beef4a63e1b19b90d85d89f0
harigovind-dot-a <harigovind@dotproduct.in>
2025-07-10 11:01:33 +0530

    CLass-Based Views

--- budgettracker/urls.py
+++ budgettracker/urls.py
@@ -1,6 +1,6 @@
 from rest_framework.routers import DefaultRouter
 from django.urls import path, include
-from .views import BudgetViewSet, TransactionViewSet, CategoryViewSet, budget_status_view
+from .views import BudgetViewSet, TransactionViewSet, CategoryViewSet, BudgetStatusView
 
 router = DefaultRouter()
 router.register(r'budgets', BudgetViewSet, basename='budget')
@@ -9,6 +9,6 @@ router.register(r'categories', CategoryViewSet, basename='category')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('budget-status/', budget_status_view, name='budget-status'),
+    path('budget-status/', BudgetStatusView.as_view(), name='budget-status'),
     
 ]
